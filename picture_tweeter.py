2495da5c92f8253fd958a7420314f77dea98c6c3
Baptiste <baptiste.colin@outlook.com>
2018-01-13 23:27:30 +0100

    adds twitter authentication

--- picture_tweeter.py
+++ picture_tweeter.py
@@ -1,1 +1,18 @@
 # Script that takes a picture path as an argument, and then tweets it
+import twitter
+import json
+import sys, os
+
+pic_path = sys.argv[1]
+
+credentials = json.loads(open("credentials.json").read())
+
+try:
+    api = twitter.Api(consumer_key=credentials["key"],
+            consumer_secret=credentials["secret"],
+            access_token_key=credentials["access_token"],
+            access_token_secret=credentials["access_token_secret"])
+
+except twitter.TwitterHTTPError as twittererror:
+    print('authentication failed :-\(')
+
