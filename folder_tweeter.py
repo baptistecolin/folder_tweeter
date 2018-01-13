749d1e139b921f9eeeb622cab3064de7b42b71c4
Baptiste <baptiste.colin@outlook.com>
2018-01-14 00:14:19 +0100

    adds folder_tweeter.py that includes some stuff to properly manage .already_tweeted.json

--- folder_tweeter.py
+++ folder_tweeter.py
@@ -1,1 +1,16 @@
 # Script that should tweet one of the picture of the folder that has not been tweeted yet
+import os
+import json
+
+if not(os.path.isfile("./.already_tweeted.json")):
+    already_tweeted = {}
+    already_tweeted['data'] = []
+
+else:
+    with open(".already_tweeted.json", 'r') as f:
+        already_tweeted = json.load(f)
+    print(already_tweeted)
+
+with open(".already_tweeted.json", 'w') as f:
+    json.dump(already_tweeted, f)
+
