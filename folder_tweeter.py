e86311a27b03c64bb8133b98f7dc9642ba190be4
Baptiste <baptiste.colin@outlook.com>
2018-01-14 13:41:30 +0100

    builds an array made of all the picture names

--- folder_tweeter.py
+++ folder_tweeter.py
@@ -1,6 +1,7 @@
 # Script that should tweet one of the picture of the folder that has not been tweeted yet
-import os
+import os, sys
 import json
+import re
 
 if not(os.path.isfile("./.already_tweeted.json")):
     already_tweeted = {}
@@ -10,6 +11,8 @@ else:
     with open(".already_tweeted.json", 'r') as f:
         already_tweeted = json.load(f)
     print(already_tweeted)
+
+all_pics = [ filename for filename in os.listdir(path) if re.match(r"(.)+.(png|jpg|JPG|PNG)$", filename) ]
 
 with open(".already_tweeted.json", 'w') as f:
     json.dump(already_tweeted, f)
