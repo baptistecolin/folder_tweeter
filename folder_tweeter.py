7a2e26d07befef43ef75c258a976d5d0e63f54b7
Baptiste <baptiste.colin@outlook.com>
2018-01-14 13:46:36 +0100

    comments

--- folder_tweeter.py
+++ folder_tweeter.py
@@ -3,15 +3,19 @@ import os, sys
 import json
 import re
 
+# managing the .already_tweeted.json file, that keeps track of what's have been tweeted so far
 if not(os.path.isfile("./.already_tweeted.json")):
+    # if there's no such file, let's create an empty one
     already_tweeted = {}
     already_tweeted['data'] = []
 
 else:
+    # if there's one, we just have to open it
     with open(".already_tweeted.json", 'r') as f:
         already_tweeted = json.load(f)
     print(already_tweeted)
 
+# building a list of all the picture files that are in the specified folder
 all_pics = [ filename for filename in os.listdir(path) if re.match(r"(.)+.(png|jpg|JPG|PNG)$", filename) ]
 
 with open(".already_tweeted.json", 'w') as f:
