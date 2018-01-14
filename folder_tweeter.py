5de38d3a4344d3c86e332f6b9ffe42de81613e8a
Baptiste <baptiste.colin@outlook.com>
2018-01-14 13:53:53 +0100

    adds a security check

--- folder_tweeter.py
+++ folder_tweeter.py
@@ -10,12 +10,16 @@ if not(os.path.isfile("./.already_tweeted.json")):
     # if there's no such file, let's create an empty one
     already_tweeted = {}
     already_tweeted['data'] = []
-
+    already_tweeted['folder_path'] = path
 else:
     # if there's one, we just have to open it
     with open(".already_tweeted.json", 'r') as f:
         already_tweeted = json.load(f)
-    print(already_tweeted)
+        
+        # the opened .already_tweeted.json folder sould correspond to to the folder we are working with
+        # the program should fail if they do not correspond
+        # TODO : make it so several .already_tweeted.json files can be handled
+        assert path == already_tweeted['folder_path']     
 
 # building a list of all the picture files that are in the specified folder
 all_pics = [ filename for filename in os.listdir(path) if re.match(r"(.)+.(png|jpg|JPG|PNG)$", filename) ]
